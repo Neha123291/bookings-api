9829baf71c6c058238f2b7276870be07e2eab6f5
Neha Karmakar <nkca1998@gmail.com>
2025-07-03 09:42:03 -0300

    updated user Id in booking to dynamic

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.get('/test', (req, res) => {
   res.send('Test route works!');
 });
 
+const authRoutes = require('./routes/auth');
+app.use('/auth', authRoutes);  
+
+
 // Start server
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
